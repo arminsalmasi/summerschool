b83816fcc07a1e087dd3992b581d9c024cba9dcd
armin <arminsalmasi@gmail.com>
2017-06-28 18:56:16 +0200

    input arguments of get input is corrected / unnecessary variable declarations are deleted from main

--- exercises/heat/heattrans_proj/getInput.F90
+++ exercises/heat/heattrans_proj/getInput.F90
@@ -1,18 +1,23 @@
-function getInput(nx, ny, dx, dy)
-  implicit none
+module getInput
 
-  integer :: nx, ny
-  real :: dx,dy
+use heattrans_datastruc
+  
+  contains
+    function getInput(nx,ny,dx,dy,bottomBC,tophBC,rightBC,leftBC,midVal)
+      implicit none
 
-  write(*,*) "enter nx"
-  read(*,*) nx
-  write(*,*) "enter ny"
-  read(*,*) ny
-  write(*,*) "enter dx"
-  read(*,*) dx
-  write(*,*) "enter dy" 
-  read(*,*) dy
+ 
+      write(*,*) "enter nx"
+      read(*,*) nx
 
+      write(*,*) "enter ny"
+      read(*,*) ny
+
+      write(*,*) "enter dx"
+      read(*,*) dx
+
+      write(*,*) "enter dy" 
+      read(*,*) dy
 
   end function getInput
-  
+end module getgInput  
--- exercises/heat/heattrans_proj/main.F90
+++ exercises/heat/heattrans_proj/main.F90
@@ -3,16 +3,12 @@ program heatTransfer
   use heattrans_datastruct
 
   implicit none
-  logical:: input_err, out_err_1, out_err_2
 
-  
-
-  
-  input_err=getInput(nx,ny,dx,dy,bottomBC,tophBC,rightBC,leftBC,midVal)
+  input_err= getInput(nx,ny,dx,dy,bottomBC,tophBC,rightBC,leftBC,midVal)
   prevGrid = doInitGrid(nx,ny,bottomBC,tophBC,rightBC,leftBC,midVal)
-  lapl = doCalcLapl(prevGrid)
+  lapl     = doCalcLapl(prevGrid)
   currGrid = doAppyLapl(prevGrid, Lapl)
-  out1 = doPrintOutput(prevGrid)
-  out2 = doPrintOutput(currGrid)
+  out1     = doPrintOutput(prevGrid)
+  out2     = doPrintOutput(currGrid)
   
 end program heatTransfer      
