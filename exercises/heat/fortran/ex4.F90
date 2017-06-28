44ef9783ee82be6cb6a2204dc9f5a703c883e4c9
arminsalmasi <arminsalmasi@gmail.com>
2017-06-28 14:05:01 +0300

    ex4 for heat transfer probelm is added laplacian initialization part is imported from the laplacian example

---
+++ exercises/heat/fortran/ex4.F90
@@ -0,0 +1,47 @@
+program ex4
+
+  implicit none
+
+  integer, parameter :: nx = 10, ny = 10
+  real, dimension(nx, ny) :: prev, lapl
+  integer :: i, j
+
+  real, parameter :: dx = 0.01, dy = 0.01
+
+
+
+! initialize prev array with varying boundaries
+  prev(:,:)  = 65.0 ! middle
+  prev(:,1)  = 20.0 ! top
+  prev(:,ny) = 70.0 ! bottom
+  prev(1,:)  = 85.0 ! left
+  prev(nx,:) = 5.0  ! right
+
+! initialize lapl array to zeros
+  lapl(:,:)  = 0.0 ! middle
+
+!-------------------------------------------------- 
+
+
+  ! TODO: implement Laplacian in double do-loop using prev 
+  ! and saving to lapl array. Remember to evaluate it only
+  ! at the inner points.
+
+
+
+
+
+
+!--------------------------------------------------
+! printing of the prev and lapl arrays
+  write(*,*) "Previous array:"
+  do j = 1, ny
+     write(*,'(*(F5.1))') prev(:,j)
+  end do
+
+  write(*,*) "Laplacian of the array:"
+  do j = 1, ny
+      write(*,'(*(F5.1))') lapl(:,j)
+  end do
+
+end program ex4
