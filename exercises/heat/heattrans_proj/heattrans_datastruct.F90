e8b4ff0a74f368111eb73e864f7f47f271b5d29e
armin <arminsalmasi@gmail.com>
2017-06-28 18:49:44 +0200

    MAin* is renamed to main / some varianbales are added to datastruc

--- exercises/heat/heattrans_proj/heattrans_datastruct.F90
+++ exercises/heat/heattrans_proj/heattrans_datastruct.F90
@@ -4,12 +4,12 @@ Public
   
   integer :: nx, ny
   real :: dx, dy, bottomBC, topBC, leftBC, rightBC, midVal
-  logical :: input_err, out_errq, out_err2
+  logical :: in_err, out_errq, out_err2
   
 
   type grid
      real, allocatable, save :: prevGrid(:,:)
-     real, allocatabel, save :: lapl(:,:)
+     real, allocatable, save :: lapl(:,:)
      real, allocatable, save :: currGrid(:,:)
   end type grid
 
