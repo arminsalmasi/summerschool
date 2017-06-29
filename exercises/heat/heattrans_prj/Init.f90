54bda73d1ba8345ead3c2d3aea7fcec3f786737d
arminsalmasi <arminsalmasi@gmail.com>
2017-06-29 14:54:16 +0200

    init method with bug

--- exercises/heat/heattrans_prj/Init.f90
+++ exercises/heat/heattrans_prj/Init.f90
@@ -4,5 +4,24 @@
 ! and open the template in the editor.
 !
 module Init
+      !  use dataStruc
+
+ contains   
+     function doInitGrid(xnum,ynum,deltax,deltay,BCbot,BCtop,BCright,BCleft,ValsMid)
+
+        implicit none
+        integer :: xNum, yNum
+        real :: deltaX,deltaY, bcBot,bcTop,bcRight,bcLeft,valsMid
+        gr%prevGrid(xNum,yNum) :: doInitGrid
+        
+        
+        !allocate grid(xNum,yNum)
+        allocate doInitGrid.prevGrid(xNum,yNum) 
+        doInitGrid(:,:)  = valsMid ! middle
+        doInitGrid(:,1)  = BCtop ! top
+        doInitGrid(:,ny) = BCbot ! bottom
+        doInitGrid(1,:)  = BCleft ! left
+        doInitGrid(nx,:) = BCright  ! right
     
+    endfunction doInitGrid    
 endmodule init
