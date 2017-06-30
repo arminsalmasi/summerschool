15d14cbbe257c50102f69ee2323e13d5213852aa
arminsalmasi <arminsalmasi@gmail.com>
2017-06-30 09:57:48 +0200

    some practice on shred and private concept

--- exercises/openmp/fortran/data-sharing/variables.F90
+++ exercises/openmp/fortran/data-sharing/variables.F90
@@ -1,15 +1,15 @@
 program exer1
   implicit none
-  integer :: var1, var2
+  real :: var1, var2
   var1 = 1
   var2 = 2
+  print *, 'Region 0:       var1=', var1, 'var2=', var2
 
-  ! TODO:
-  !   Test different data sharing clauses here
+  !$omp parallel private(var2)
   print *, 'Region 1:       var1=', var1, 'var2=', var2
   var1 = var1 + 1
   var2 = var2 + 1
-  !end here :)
+  !$omp end parallel
   print *, 'After region 1: var1=', var1, 'var2=', var2
   print *
 
