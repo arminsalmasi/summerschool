3b8f14fc17b4122077936ddf122ee46a3bbf3e43
arminsalmasi <arminsalmasi@gmail.com>
2017-06-30 11:12:14 +0200

    ex on sum reduction

--- exercises/openmp/fortran/work-sharing/sum.F90
+++ exercises/openmp/fortran/work-sharing/sum.F90
@@ -1,11 +1,12 @@
 program vectorsum
+use omp_lib  
   implicit none
   integer, parameter :: rk = kind(1d0)
   integer, parameter :: ik = selected_int_kind(9)
   integer, parameter :: nx = 102400
 
   real(kind=rk), dimension(nx) :: vecA, vecB, vecC
-  real(kind=rk)    :: sum
+  real(kind=rk)    :: sum , starttime, endtime
   integer(kind=ik) :: i
 
   ! Initialization of vectors
@@ -14,11 +15,14 @@ program vectorsum
      vecB(i) = vecA(i)**2
   end do
 
-  ! TODO:
-  !   Implement here the parallelized version of vector addition,
-  !   vecC = vecA + vecB
-
+  starttime=omp_get_wtime()
+  !$omp parallel do shared(vecA,vecB) PRIVATE(i)
+  do i = 1 , size(vecA) 
+     vecC(i) = vecA(i) ** vecB(i)
+  enddo   
   ! Compute the check value
+  !$omp end parallel do
+  endtime=omp_get_wtime()
   write(*,*) 'Reduction sum: ', sum(vecC)
-
+  write(*,*) 'time: ' , endtime-starttime
 end program vectorsum
