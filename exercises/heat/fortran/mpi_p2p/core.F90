2f2a695cdeb5dd22eeec4b22955cf64995128822
arminsalmasi <arminsalmasi@gmail.com>
2017-07-02 15:28:17 +0200

    start to implelkemnt isend and irecv will be compiled on sisu

--- exercises/heat/fortran/mpi_p2p/core.F90
+++ exercises/heat/fortran/mpi_p2p/core.F90
@@ -14,23 +14,47 @@ contains
     type(parallel_data), intent(in) :: parallel
       
     integer :: status(MPI_STATUS_SIZE)
-    integer :: ierr
+    integer :: ierr, req10,req11,req12,req13
     real, allocatable :: msg(:,:)
 
 
 
     ! TODO start: implement halo exchange
+    !##################################################################################
+    ! task sendrecv
+       
+    ! Send to left, receive from right
+     
+      !call mpi_sendrecv(field0%data(0,1),field0%nx+2,&
+      !MPI_DOUBLE_PRECISION, parallel%nleft,10,field0%data(0,field0%ny+1),field0%nx+2,&
+      !MPI_DOUBLE_PRECISION, parallel%nright,10,MPI_COMM_WORLD, MPI_STATUS_IGNORE, ierr)
+        
+    ! Send to right, receive from left
+
+      !call mpi_sendrecv(field0%data(0,field0%ny),field0%nx+2,&
+      !MPI_DOUBLE_PRECISION, parallel%nright,11,field0%data(0,0),field0%nx+2, &
+      !MPI_DOUBLE_PRECISION, parallel%nleft, 11,MPI_COMM_WORLD, MPI_STATUS_IGNORE, ierr)
+       
+    !###################################################################################
+    ! task isen irec
     
     ! Send to left, receive from right
-      !msg = field0(:,1)
-      call mpi_sendrecv(field0%data(0,1),field0%nx+2,&
-      MPI_DOUBLE_PRECISION, parallel%nleft,10,field0%data(0,field0%ny+1),field0%nx+2,&
-      MPI_DOUBLE_PRECISION, parallel%nright,10,MPI_COMM_WORLD, MPI_STATUS_IGNORE, ierr)
+      
+      call mpi_isend(field0%data(0,1),field%nx+2, MPI_DOUBLE_PRECISION, parallel%nleft, 10, MPI_COMM_WORLD, req10, ierr)
+      call mpi_irecv(field0%data(0,field0%ny+1),field0%nx+2,MPI_DOUBLE_PRECISION, parallel%nright,10,MPI_COMM_WORLD, req11, ierr)
+
     ! Send to right, receive from left
-      !msg = field0(:,field0%ny)
-      call mpi_sendrecv(field0%data(0,field0%ny),field0%nx+2,&
-      MPI_DOUBLE_PRECISION, parallel%nright,11,field0%data(0,0),field0%nx+2, &
-      MPI_DOUBLE_PRECISION, parallel%nleft, 11,MPI_COMM_WORLD, MPI_STATUS_IGNORE, ierr)
+      
+      call mpi_isend(field0%data(0,field0%ny),field0%nx+2,MPI_DOUBLE_PRECISION, parallel%nright,11,MPI_COMM_WORLD, req12, ierr)
+      call mpi_irecv(field0%data(0,0),field0%nx+2,MPI_DOUBLE_PRECISION, parallel%nleft, 11,MPI_COMM_WORLD, req13, ierr)
+      
+
+
+      mpi_waitall(4, (/req10,req11,req12,req13/), status)
+    !##################################################################################
+
+          
+         
     ! TODO end
 
   end subroutine exchange
