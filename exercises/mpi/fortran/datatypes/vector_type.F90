119c2ef3712c4b74258678c3e59d03a9dc1294a9
arminsalmasi <arminsalmasi@gmail.com>
2017-07-03 10:03:26 +0200

    ex 10-a datatype passing is done

--- exercises/mpi/fortran/datatypes/vector_type.F90
+++ exercises/mpi/fortran/datatypes/vector_type.F90
@@ -5,8 +5,10 @@ program datatype1
   integer, dimension(8,8) :: array
   integer :: rank, ierr
   !TODO: declare variable for datatype
-  integer :: i, j
-
+   integer :: newtype
+   integer :: i, j
+   integer :: status(MPI_STATUS_SIZE)
+    
   call mpi_init(ierr)
   call mpi_comm_rank(MPI_COMM_WORLD, rank ,ierr)
 
@@ -22,9 +24,18 @@ program datatype1
   end if
 
   !TODO: create datatype describing one row, use mpi_type_vector
-
+    call mpi_type_vector(12,2,4,MPI_INTEGER,newtype, ierr)
+    call mpi_type_commit(newtype,ierr)
   !TODO: send first row of matrix from rank 0 to 1
-
+  select case (rank)
+  
+     case (0) 
+     call mpi_send(array, 1, newtype,1,1,MPI_COMM_WORLD,ierr)
+    
+     case(1)
+     call mpi_recv(array, 1, newtype,0,1,MPI_COMM_WORLD, status, ierr)
+   
+  end select
   ! Print out the result
   if (rank == 1) then
      do i=1,8
@@ -34,6 +45,7 @@ program datatype1
 
   !TODO free datatype
 
+  call mpi_type_free(newtype,ierr)
   call mpi_finalize(ierr)
 
 end program datatype1
