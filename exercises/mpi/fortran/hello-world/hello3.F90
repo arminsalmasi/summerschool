670a02fb08ca0cc887616166cd3622ec6f984d9e
arminsalmasi <arminsalmasi@gmail.com>
2017-07-04 12:09:00 +0200

    initialize hello3 for ex 1 in hybrid exs.

---
+++ exercises/mpi/fortran/hello-world/hello3.F90
@@ -0,0 +1,17 @@
+  program hello
+  use mpi
+  implicit none
+
+  integer :: rank, rc, numproc
+  call mpi_init(rc)
+  call mpi_comm_rank(MPI_COMM_WORLD, rank, rc)
+
+  print *, 'Hello world!' ,rank
+  if (rank==0) then
+     call MPI_COMM_SIZE(MPI_COMM_WORLD, numproc, rc)
+     write(*,*) 'number of ranks',numproc
+  endif
+  print *, 'X'
+  call mpi_finalize(rc)
+
+end program hello
