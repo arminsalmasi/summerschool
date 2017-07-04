993b3dc298342d8bcf1f7679186643cd8307e701
arminsalmasi <arminsalmasi@gmail.com>
2017-07-04 15:37:09 +0300

    hyb ex 1 done

--- exercises/mpi/fortran/hello-world/hello3.F90
+++ exercises/mpi/fortran/hello-world/hello3.F90
@@ -1,17 +1,26 @@
   program hello
   use mpi
+  !use omp
   implicit none
 
-  integer :: rank, rc, numproc
-  call mpi_init_thread(rc)
-  call mpi_comm_rank(MPI_COMM_WORLD, rank, rc)
+  integer :: rank, rc, numproc, prov, numt, tnum
+  
+  call mpi_init_thread(mpi_thread_funneled, prov, rc)
+  
 
-  print *, 'Hello world!' ,rank
-  if (rank==0) then
-     call MPI_COMM_SIZE(MPI_COMM_WORLD, numproc, rc)
-     write(*,*) 'number of ranks',numproc
-  endif
-  print *, 'X'
+  call mpi_comm_rank(MPI_COMM_WORLD, rank, rc)
+  !$omp parallel private(numt,tnum) 
+    numt = omp_get_num_threads()
+    tnum = omp_get_thread_num()
+    print *, 'Hello world!' ,rank
+    if (rank==0) then
+      call MPI_COMM_SIZE(MPI_COMM_WORLD, numproc, rc)
+      write(*,*) 'number of ranks',numproc, 'numt', numt, 'tnum', tnum
+    endif
+    !print *, 'X'
+  
+  !$omp end parallel
+ 
   call mpi_finalize(rc)
 
 end program hello
