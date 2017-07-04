d433a909e1637dac19f26c95fc1d2966db1d2b18
arminsalmasi <arminsalmasi@gmail.com>
2017-07-04 13:10:06 +0200

    new mpi_p2p forlder for ex 4 hy

---
+++ exercises/heat/fortran/mpi_p2p_ex4_hy/core.F90
@@ -0,0 +1,96 @@
+! Main solver routines for heat equation solver
+module core
+    use heat
+
+contains
+
+  ! Exchange the boundary data between MPI tasks
+  subroutine exchange(field0, parallel)
+    use mpi
+
+    implicit none
+
+    type(field), intent(inout) :: field0
+    type(parallel_data), intent(in) :: parallel
+      
+    integer :: status(MPI_STATUS_SIZE)
+    integer :: ierr, req10,req11,req12,req13
+    real, allocatable :: msg(:,:)
+
+
+
+    ! TODO start: implement halo exchange
+    !##################################################################################
+    ! task sendrecv
+       
+    ! Send to left, receive from right
+     
+      !call mpi_sendrecv(field0%data(0,1),field0%nx+2,&
+      !MPI_DOUBLE_PRECISION, parallel%nleft,10,field0%data(0,field0%ny+1),field0%nx+2,&
+      !MPI_DOUBLE_PRECISION, parallel%nright,10,MPI_COMM_WORLD, MPI_STATUS_IGNORE, ierr)
+        
+    ! Send to right, receive from left
+
+      !call mpi_sendrecv(field0%data(0,field0%ny),field0%nx+2,&
+      !MPI_DOUBLE_PRECISION, parallel%nright,11,field0%data(0,0),field0%nx+2, &
+      !MPI_DOUBLE_PRECISION, parallel%nleft, 11,MPI_COMM_WORLD, MPI_STATUS_IGNORE, ierr)
+       
+    !###################################################################################
+    ! task isen irec
+    
+    ! Send to left, receive from right
+      
+      call mpi_isend(field0%data(0,1),field0%nx+2,MPI_DOUBLE_PRECISION,&
+                     parallel%nleft,10,MPI_COMM_WORLD,req10,ierr)
+      call mpi_irecv(field0%data(0,field0%ny+1),field0%nx+2,MPI_DOUBLE_PRECISION,&
+                     parallel%nright,10,MPI_COMM_WORLD,req11, ierr)
+
+    ! Send to right, receive from left
+      
+      call mpi_isend(field0%data(0,field0%ny),field0%nx+2,MPI_DOUBLE_PRECISION,&
+                     parallel%nright,11,MPI_COMM_WORLD,req12,ierr)
+      call mpi_irecv(field0%data(0,0),field0%nx+2,MPI_DOUBLE_PRECISION,&
+                     parallel%nleft,11,MPI_COMM_WORLD,req13,ierr)
+      
+
+
+      call mpi_waitall(4, (/req10,req11,req12,req13/), status)
+    !##################################################################################
+
+          
+         
+    ! TODO end
+
+  end subroutine exchange
+
+  ! Compute one time step of temperature evolution
+  ! Arguments:
+  !   curr (type(field)): current temperature values
+  !   prev (type(field)): values from previous time step
+  !   a (real(dp)): update equation constant
+  !   dt (real(dp)): time step value
+  subroutine evolve(curr, prev, a, dt)
+
+    implicit none
+
+    type(field), intent(inout) :: curr, prev
+    real(dp) :: a, dt
+    integer :: i, j, nx, ny
+
+    nx = curr%nx
+    ny = curr%ny
+
+    !$omp parallel do     
+      do j = 1, ny
+         do i = 1, nx
+            curr%data(i, j) = prev%data(i, j) + a * dt * &
+                & ((prev%data(i-1, j) - 2.0 * prev%data(i, j) + &
+                &   prev%data(i+1, j)) / curr%dx**2 + &
+                &  (prev%data(i, j-1) - 2.0 * prev%data(i, j) + &
+                &   prev%data(i, j+1)) / curr%dy**2)
+         end do
+      end do
+  !$omp end parallel do
+  end subroutine evolve
+
+end module core
---
+++ exercises/heat/fortran/mpi_p2p_ex4_hy/main.F90
@@ -0,0 +1,74 @@
+! Heat equation solver in 2D.
+
+program heat_solve
+  use heat
+  use core
+  use io
+  use setup
+  use utilities
+  use mpi
+
+  implicit none
+  
+  real(dp), parameter :: a = 0.5 ! Diffusion constant
+  type(field) :: current, previous    ! Current and previus temperature fields
+
+  real(dp) :: dt     ! Time step
+  integer :: nsteps       ! Number of time steps
+  integer, parameter :: image_interval = 500 ! Image output interval
+
+  type(parallel_data) :: parallelization
+  integer :: ierr, prov
+
+  integer :: iter
+
+  real(kind=dp) :: start, stop ! Timers
+
+  ! TODO start: initialize MPI
+      call mpi_init_thread(mpi_thread_funneled,prov, ierr)
+      if (prov < mpi_thread_funneled) then
+    
+      end if
+      !call mpi_comm_rank(MPI_COMM_WORLD,parallelization%rank,rc)
+      !call mpi_comm_size(MPI_COMM_WORLD,parallelization%size,rc)
+  ! TODO end
+
+  call initialize(current, previous, nsteps, parallelization)
+
+  ! Draw the picture of the initial state
+  call write_field(current, 0, parallelization)
+
+  ! Largest stable time step
+  dt = current%dx**2 * current%dy**2 / &
+       & (2.0 * a * (current%dx**2 + current%dy**2))
+
+  ! Main iteration loop, save a picture every
+  ! image_interval steps
+
+  start =  mpi_wtime()
+  
+
+     do iter = 1, nsteps
+       call exchange(previous, parallelization)
+       call evolve(current, previous, a, dt)
+       if (mod(iter, image_interval) == 0) then
+          call write_field(current, iter, parallelization)
+       end if
+       call swap_fields(current, previous)
+     end do
+
+
+  stop = mpi_wtime()
+
+  if (parallelization % rank == 0) then
+     write(*,'(A,F7.3,A)') 'Iteration took ', stop - start, ' seconds.'
+     write(*,'(A,G0)') 'Reference value at 5,5: ', previous % data(5,5)
+  end if
+  
+  call finalize(current, previous)
+
+  ! TODO start: finalize MPI
+      call mpi_finalize(ierr)
+  ! TODO end
+
+end program heat_solve
